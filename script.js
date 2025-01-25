4dd77000e575f297e2098cde8f45d58fc64232d2
Kumar Anurag <me@kmranrg.com>
2025-01-25 13:25:59 -0700

    pwa compatible

--- script.js
+++ script.js
@@ -165,3 +165,10 @@ function showDeveloperInfo() {
     footerMessage.classList.remove("show");
   }, 5000);
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(() => console.log('Service Worker registered successfully.'))
+    .catch((error) => console.error('Service Worker registration failed:', error));
+}
